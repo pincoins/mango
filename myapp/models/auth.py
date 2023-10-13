148e7871bb77f100c1a65320ae851a3339f3c068
mairoo <jonghwa@pincoin.co.kr>
2023-10-13 10:48:55 +0900

    profile model added

--- myapp/models/auth.py
+++ myapp/models/auth.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.db import models
 from model_utils import models as model_utils_models
 
@@ -54,3 +56,132 @@ class Role(models.Model):
         verbose_name = 'role'
         verbose_name_plural = 'roles'
         db_table = 'role'
+
+
+class Profile(model_utils_models.TimeStampedModel):
+    user = models.OneToOneField(
+        'myapp.User',
+        db_index=True,
+        null=True,
+        blank=True,
+        editable=True,
+        on_delete=models.SET_NULL,
+    )
+
+    phone = models.CharField(
+        max_length=16,
+        blank=True,
+        null=True,
+    )
+
+    address = models.CharField(
+        max_length=255,
+        blank=True,
+    )
+
+    phone_verified = models.BooleanField(
+        default=False,
+    )
+
+    phone_verified_status = models.IntegerField(
+        default=0,
+        db_index=True,
+    )
+
+    document_verified = models.IntegerField(
+        default=0,
+        db_index=True,
+    )
+
+    allow_order = models.BooleanField(
+        default=False,
+    )
+
+    photo_id = models.CharField(
+        max_length=256,
+        blank=True,
+    )
+
+    card = models.CharField(
+        max_length=256,
+        blank=True,
+    )
+
+    total_order_count = models.IntegerField(
+        default=0,
+    )
+
+    first_purchased = models.DateTimeField(
+        null=True,
+    )
+
+    last_purchased = models.DateTimeField(
+        null=True,
+    )
+
+    not_purchased_months = models.BooleanField(
+        default=False,
+    )
+
+    repurchased = models.DateTimeField(
+        null=True,
+    )
+
+    max_price = models.DecimalField(
+        max_digits=11,
+        decimal_places=2,
+        default=Decimal('0.00'),
+    )
+
+    total_list_price = models.DecimalField(
+        max_digits=11,
+        decimal_places=2,
+        default=Decimal('0.00'),
+    )
+
+    total_selling_price = models.DecimalField(
+        max_digits=11,
+        decimal_places=2,
+        default=Decimal('0.00'),
+    )
+
+    average_price = models.DecimalField(
+        max_digits=11,
+        decimal_places=2,
+        default=Decimal('0.00'),
+    )
+
+    mileage = models.DecimalField(
+        max_digits=11,
+        decimal_places=2,
+        default=Decimal('0.00'),
+    )
+
+    memo = models.TextField(
+        blank=True,
+    )
+
+    date_of_birth = models.DateField(
+        blank=True,
+        null=True,
+    )
+
+    gender = models.IntegerField(
+        default=0,
+        db_index=True,
+    )
+
+    domestic = models.IntegerField(
+        default=0,
+        db_index=True,
+    )
+
+    telecom = models.CharField(
+        max_length=16,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'profile'
+        verbose_name_plural = 'profiles'
+        db_table = 'profile'
